786fd337ce5eb4e412df5229e24c3020ec10f8db
Napster <napster@npstr.space>
2017-08-01 01:04:22 +0200

    Minor fixes/Convenience stuff/JPQL queries

--- FredBoat/src/main/java/fredboat/event/EventListenerBoat.java
+++ FredBoat/src/main/java/fredboat/event/EventListenerBoat.java
@@ -217,7 +217,7 @@ public class EventListenerBoat extends AbstractEventListener {
                 && player.getPlayingTrack() != null
                 && joinedChannel.getMembers().contains(guild.getSelfMember())
                 && player.getHumanUsersInCurrentVC().size() > 0
-                && EntityReader.getEntity(guild.getId(), GuildConfig.class).isAutoResume()
+                && EntityReader.getOrCreateEntity(guild.getId(), GuildConfig.class).isAutoResume()
                 ) {
             CentralMessaging.sendMessage(player.getActiveTextChannel(), I18n.get(guild).getString("eventAutoResumed"));
             player.setPause(false);
